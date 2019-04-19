c745faa9b16206426465156da5d8aafec63f693d
Farhad <farhad9801@gmail.com>
2019-04-19 12:13:16 +0400

    utf-8

--- src/controllers/api/application_controller.cr
+++ src/controllers/api/application_controller.cr
@@ -1,7 +1,7 @@
 class API::ApplicationController < ApplicationController
   before_action do
     all do
-      context.response.content_type = "application/json"
+      context.response.content_type = "application/json; charset=utf-8"
     end
   end
 end
